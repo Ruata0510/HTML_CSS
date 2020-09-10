e4f7eaeee5592f10fe8718e22d82c9167b124b40
svetlana titova <svetlanka65@inbox.ru>
2020-09-10 10:45:24 +0300

    5 shag

--- Canvas_Game/game.js
+++ Canvas_Game/game.js
@@ -13,6 +13,19 @@ let game = {
 	},
 	init: function() {
 		this.ctx = document.getElementById("mycanvas").getContext("2d");
+		this.setEvents();
+	},
+	setEvents() {
+		window.addEventListener("keydown", e => {
+			if (e.keyCode === 37) {
+				this.platform.dx = -this.platform.velocity;
+			} else if (e.keyCode === 39) {
+				this.platform.dx = this.platform.velocity;
+			}
+		});
+		window.addEventListener("keyup", e => {
+			this.platform.dx = 0;
+		});
 	},
 	preload(callback) {
 		let loaded = 0;
@@ -40,15 +53,19 @@ let game = {
 			}
 		}
 	},
+	update() {
+		this.platform.move();
+	},
 	run() {
 		window.requestAnimationFrame(() => {
+			this.update();
 			this.render();
+			this.run();
 		});
 	},
 	render() {
 		this.ctx.drawImage(this.sprites.background, 0, 0);
-		this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
-		 this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+		this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
 		this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
 		this.renderBlocks();
 	},
@@ -74,10 +91,17 @@ game.ball = {
 };
 
 game.platform = {
+    velocity: 6,
+    dx: 0,
     x: 280,
-    y: 300
+    y: 300,
+    move() {
+    	if (this.dx) {
+    		this.x += this.dx;
+    	}
+    }
 };
 
 window.addEventListener("load", () => {
-game.start();
+	game.start();
 });
